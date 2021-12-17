c4ee2350550b2c92e02c2c87cb657364fe0139eb
Elias <elias@debian-BULLSEYE-live-builder-AMD64>
2021-12-17 12:38:34 -0600

    added shadow box

--- pages/Store.js
+++ pages/Store.js
@@ -10,163 +10,162 @@ const Store = () => {
       <NavBar />
       <div className={styles.container}>
         <Row className="d-flex justify-content-center">
-          <Col xs={12} className="p-3 shadow mb-3 bg-white roundede">
+          <Col xs={12} className="p-3 shadow-lg mb-3 bg-white roundede">
             <h3>Promociones</h3>
           </Col>
           <Col xs={12} className="p-3 mb-3">
             <Marque />
           </Col>
-          <Col xs={12} className="p-3 shadow mb-3 bg-white roundede">
+          <Col xs={12} className="p-3 shadow-lg mb-3 bg-white roundede">
             <h3>Recomendado</h3>
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"flautas.jpeg"}
               details={"Es una prueba"}
               titulo={"Flautas"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"tacos.jpeg"}
               details={"Es una prueba"}
               titulo={"Tacos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"chileNahogada.jpeg"}
               details={"Es una prueba"}
               titulo={"Chile"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"molePollo.jpeg"}
               details={"Es una prueba"}
               titulo={"Mole"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"pozole.jpeg"}
               details={"Es una prueba"}
               titulo={"Posolazo"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"Tamales.jpeg"}
               details={"Es una prueba"}
               titulo={"Tamalitos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"zope.jpeg"}
               details={"Es una prueba"}
               titulo={"Sope"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"flautas.jpeg"}
               details={"Es una prueba"}
               titulo={"Flautas"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"tacos.jpeg"}
               details={"Es una prueba"}
               titulo={"Tacos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"chileNahogada.jpeg"}
               details={"Es una prueba"}
               titulo={"Chile"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"molePollo.jpeg"}
               details={"Es una prueba"}
               titulo={"Mole"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"pozole.jpeg"}
               details={"Es una prueba"}
               titulo={"Posolazo"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"Tamales.jpeg"}
               details={"Es una prueba"}
               titulo={"Tamalitos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"zope.jpeg"}
               details={"Es una prueba"}
               titulo={"Sope"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"flautas.jpeg"}
               details={"Es una prueba"}
               titulo={"Flautas"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"tacos.jpeg"}
               details={"Es una prueba"}
               titulo={"Tacos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"chileNahogada.jpeg"}
               details={"Es una prueba"}
               titulo={"Chile"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"molePollo.jpeg"}
               details={"Es una prueba"}
               titulo={"Mole"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"pozole.jpeg"}
               details={"Es una prueba"}
               titulo={"Posolazo"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"Tamales.jpeg"}
               details={"Es una prueba"}
               titulo={"Tamalitos"}
             />
           </Col>
-          <Col xs={12} md={4}>
+          <Col xs={12} md={4} lg={3}>
             <ItemCard
               image={"zope.jpeg"}
               details={"Es una prueba"}
               titulo={"Sope"}
             />
           </Col>
-          
         </Row>
       </div>
     </>
