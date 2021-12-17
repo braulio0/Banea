4a524c368379a0a4e42c3ab5aeb9848152bc4b1d
Elias <elias@debian-BULLSEYE-live-builder-AMD64>
2021-12-17 12:42:48 -0600

    fixed shadows

--- Componets/Map.js
+++ Componets/Map.js
@@ -2,7 +2,8 @@ import Head from "next/head";
 
 const Map = () => {
   return (
-    <iframe
+    <iframe 
+    className="shadow-lg mb-3 bg-white rounded"
       src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d120493.84257056678!2d-99.12069835361275!3d19.30702650693737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sfondas!5e0!3m2!1ses-419!2smx!4v1639689011425!5m2!1ses-419!2smx"
       style={{
         width: "100%",
--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,12 @@ export default function Home() {
         <Map> </Map>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={styles.footer} >
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+          
         >
           Powered by
           <span className={styles.logo}>
